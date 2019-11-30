b7cc0b7bde1afa9e467ed066c027f030a691de7a
phonght32 <thanhphongho1998@gmail.com>
2019-11-30 22:42:12 +0700

    add uart mapping table

--- stm32f407/components/driver/include/uart.h
+++ stm32f407/components/driver/include/uart.h
@@ -5,6 +5,44 @@
 extern "C" {
 #endif
 
+#include "stdint.h"
+
+/* NOTE: Change HSE_VALUE in stm32f4xx.h to 8000000 before start your application */    
+
+/* Table below shows all possible pins for each uart 
+ *
+ *  U(S)ARTx | Pins pack 1 | Pins pack 2 | Pins pack 3 |  APB  | 
+ *           |  TX    RX   |  TX    RX   |  TX    RX   |       |
+ *--------------------------------------------------------------
+ *  USART1   |  PA9   PA10 |  PB6   PB7  |  -     -    |   2   |
+ *  USART2   |  PA2   PA3  |  PD5   PD6  |  -     -    |   1   |
+ *  USART3   |  PB10  PB11 |  PC10  PC11 |  PD8   PD9  |   1   |
+ *  USART4   |  PA0   PA1  |  PC10  PC11 |  -     -    |   1   |
+ *  USART5   |  PC12  PD2  |  -     -    |  -     -    |   1   |
+ *  USART6   |  PC6   PC7  |  PG14  PG9  |  -     -    |   2   |
+ *  USART7   |  PE8   PE7  |  PF7   PF6  |  -     -    |   1   |
+ *  USART8   |  PE1   PE0  |  -     -    |  -     -    |   1   |
+ */
+
+typedef enum {
+    USART_NUM_1 = 0,
+    USART_NUM_2,
+    USART_NUM_3,
+    USART_NUM_4,
+    USART_NUM_5,
+    USART_NUM_6,
+    USART_NUM_7,
+    USART_NUM_8,
+    USART_NUM_MAX
+}usart_num_t;
+
+typedef enum {
+    USART_PINS_PACK_1 = 0,
+    USART_PINS_PACK_2,
+    USART_PINS_PACK_3,
+    USART_PINS_PACK_MAX
+} usart_pins_pack_t;
+
 #ifdef __cplusplus
 }
 #endif
--- stm32f407/components/driver/uart.c
+++ stm32f407/components/driver/uart.c
@@ -1,4 +1,10 @@
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_rcc.h"
+
+#include "stdlib.h"
+
 #include "include/uart.h"
 
 
@@ -9,7 +15,37 @@
 
 
 /* Internal variable ---------------------------------------------------------*/
+uint32_t UART_PARAM_MAPPING[USART_PINS_PACK_MAX][USART_NUM_MAX] = {
+    {   {(uint32_t)GPIOA,  GPIO_Pin_9, RCC_AHB1Periph_GPIOA,  GPIO_PinSource9, (uint32_t)GPIOA, GPIO_Pin_10, RCC_AHB1Periph_GPIOA, GPIO_PinSource10, GPIO_AF_USART1},
+        {(uint32_t)GPIOA,  GPIO_Pin_2, RCC_AHB1Periph_GPIOA,  GPIO_PinSource2, (uint32_t)GPIOA,  GPIO_Pin_3, RCC_AHB1Periph_GPIOA,  GPIO_PinSource3, GPIO_AF_USART2},
+        {(uint32_t)GPIOB, GPIO_Pin_10, RCC_AHB1Periph_GPIOB, GPIO_PinSource10, (uint32_t)GPIOB, GPIO_Pin_11, RCC_AHB1Periph_GPIOB, GPIO_PinSource11, GPIO_AF_USART3},
+        {(uint32_t)GPIOA,  GPIO_Pin_0, RCC_AHB1Periph_GPIOA,  GPIO_PinSource0, (uint32_t)GPIOA,  GPIO_Pin_1, RCC_AHB1Periph_GPIOA,  GPIO_PinSource1,  GPIO_AF_UART4},
+        {(uint32_t)GPIOC, GPIO_Pin_12, RCC_AHB1Periph_GPIOC, GPIO_PinSource12, (uint32_t)GPIOD,  GPIO_Pin_2, RCC_AHB1Periph_GPIOD,  GPIO_PinSource2,  GPIO_AF_UART5},
+        {(uint32_t)GPIOC,  GPIO_Pin_6, RCC_AHB1Periph_GPIOC,  GPIO_PinSource6, (uint32_t)GPIOC,  GPIO_Pin_7, RCC_AHB1Periph_GPIOC,  GPIO_PinSource7, GPIO_AF_USART6},
+        {(uint32_t)GPIOE,  GPIO_Pin_8, RCC_AHB1Periph_GPIOE,  GPIO_PinSource8, (uint32_t)GPIOE,  GPIO_Pin_7, RCC_AHB1Periph_GPIOE,  GPIO_PinSource7,  GPIO_AF_UART7},
+        {(uint32_t)GPIOE,  GPIO_Pin_1, RCC_AHB1Periph_GPIOE,  GPIO_PinSource1, (uint32_t)GPIOE,  GPIO_Pin_0, RCC_AHB1Periph_GPIOE,  GPIO_PinSource0,  GPIO_AF_UART8}
+    },
 
+    {   {(uint32_t)GPIOB,  GPIO_Pin_6, RCC_AHB1Periph_GPIOB,  GPIO_PinSource6, (uint32_t)GPIOB,  GPIO_Pin_7, RCC_AHB1Periph_GPIOB,  GPIO_PinSource7, GPIO_AF_USART1},
+        {(uint32_t)GPIOD,  GPIO_Pin_5, RCC_AHB1Periph_GPIOD,  GPIO_PinSource5, (uint32_t)GPIOD,  GPIO_Pin_6, RCC_AHB1Periph_GPIOD,  GPIO_PinSource6, GPIO_AF_USART2},
+        {(uint32_t)GPIOC, GPIO_Pin_10, RCC_AHB1Periph_GPIOC, GPIO_PinSource10, (uint32_t)GPIOC, GPIO_Pin_11, RCC_AHB1Periph_GPIOC, GPIO_PinSource11, GPIO_AF_USART3},
+        {(uint32_t)GPIOC, GPIO_Pin_10, RCC_AHB1Periph_GPIOC, GPIO_PinSource10, (uint32_t)GPIOC, GPIO_Pin_11, RCC_AHB1Periph_GPIOC, GPIO_PinSource11,  GPIO_AF_UART4},
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               },
+        {(uint32_t)GPIOG, GPIO_Pin_14, RCC_AHB1Periph_GPIOG, GPIO_PinSource14, (uint32_t)GPIOG,  GPIO_Pin_9, RCC_AHB1Periph_GPIOG,  GPIO_PinSource9, GPIO_AF_USART6},
+        {(uint32_t)GPIOF,  GPIO_Pin_7, RCC_AHB1Periph_GPIOF,  GPIO_PinSource7, (uint32_t)GPIOF,  GPIO_Pin_6, RCC_AHB1Periph_GPIOF,  GPIO_PinSource6,  GPIO_AF_UART7},
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               }
+    },
+
+    {   {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               },
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               },
+        {(uint32_t)GPIOD,  GPIO_Pin_8, RCC_AHB1Periph_GPIOD,  GPIO_PinSource8, (uint32_t)GPIOD,  GPIO_Pin_9, RCC_AHB1Periph_GPIOD,  GPIO_PinSource9, GPIO_AF_USART3},
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               },
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               },
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               },
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               },
+        {               ,            ,                     ,                 ,                ,            ,                     ,                 ,               }
+    }
+}
 
 /* Internal function ---------------------------------------------------------*/
 
