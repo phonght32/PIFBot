77a451141c0d502faab8542a5dbec71a0465a1ec
phonght32 <thanhphongho1998@gmail.com>
2019-12-05 19:39:34 +0700

    update main.c

--- stm32f407/src/main.c
+++ stm32f407/src/main.c
@@ -6,7 +6,11 @@
 #include "../components/driver/include/uart.h"
 #include "../components/driver/include/timer.h"
 
-
+#define		BUFF_SIZE			4
+uint8_t 	rxbuff[BUFF_SIZE];
+uint8_t 	a[4*BUFF_SIZE];
+uint16_t	index = 0;
+uint16_t 	rcv_flag = 0;
 
 
 
@@ -39,7 +43,7 @@ void init_main(void)
   uart_dma_enable_rx(uart_handle);
 
   dma_config_t dma_config;
-//  dma_config.buffer = rxbuff;
+  dma_config.buffer = rxbuff;
   dma_config.buffer_size = 4;
   dma_config.dma_channel = DMA_CHANNEL_4;
   dma_config.dma_mode = DMA_Mode_Normal;
@@ -49,6 +53,23 @@ void init_main(void)
   dma_handle_t dma_handle = dma_init(&dma_config);
 
   dma_intr_enable(dma_handle, DMA_IT_TC);
+}
+
+void DMA1_Stream2_IRQHandler(void)
+{
+  uint16_t i;
+
+  /* Clear the DMA1_Stream2 TCIF2 pending bit */
+  DMA_ClearITPendingBit(DMA1_Stream2, DMA_IT_TCIF2);
+
+  for(i=0; i<BUFF_SIZE; i++)
+
+    a[index + i] = rxbuff[i];
+
+	index = index + BUFF_SIZE;
+  rcv_flag = 1;
+
+	DMA_Cmd(DMA1_Stream2, ENABLE);
 }
 
 
