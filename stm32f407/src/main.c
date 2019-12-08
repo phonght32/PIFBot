f9cebbcb7ccb118d06b9c3dbd903362071e8d6a0
phonght32 <thanhphongho1998@gmail.com>
2019-12-08 20:12:56 +0700

    update main.c

--- stm32f407/src/main.c
+++ stm32f407/src/main.c
@@ -1,12 +1,38 @@
 
 #include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "../components/driver_step_motor/include/tb6560.h"
+
+#include "system_timetick.h"
 
 int main(void)
 {
-	while(1)
-  {
-		
-  }
+    SysTick_Config(SystemCoreClock/100);
+
+    tb6560_config_t tb6560_cfg;
+    tb6560_cfg.pin_clk.pwm_channel = PWM_CHANNEL_2;
+    tb6560_cfg.pin_clk.pwm_pins_pack = PWM_PINS_PACK_2;
+    tb6560_cfg.pin_clk.pwm_duty = 50;
+    tb6560_cfg.pin_clk.timer = TIMER_NUM_4;
+    tb6560_cfg.pin_clk.timer_prescaler = 10000;
+    tb6560_cfg.pin_clk.timer_period = 4200;
+    tb6560_cfg.pin_dir.GPIOx = GPIOD;
+    tb6560_cfg.pin_dir.GPIO_Pin = GPIO_Pin_15;
+    tb6560_cfg.pin_dir.pull_reg = GPIO_PULL_REG_DISABLE;
+    tb6560_cfg.micro_step_div = MICRO_STEP_DIV16;
+    tb6560_handle_t tb6560_handle = tb6560_init(&tb6560_cfg);
+    tb6560_start(tb6560_handle);
+    tb6560_set_dir(tb6560_handle,1);
+
+    while (1)
+    {
+        if(tick_count == 100)
+        {
+            tick_count = 0;
+        }
+    }
+
+    return 0;
 }
 
 
