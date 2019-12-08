513f1ceefd37a95eca208c5c3040f52df76128fa
phonght32 <thanhphongho1998@gmail.com>
2019-12-08 12:47:50 +0700

    add update tb6560_init & add cleanup function

--- stm32f407/components/driver_step_motor/tb6560.c
+++ stm32f407/components/driver_step_motor/tb6560.c
@@ -11,30 +11,38 @@
 
 /* Internal variable ---------------------------------------------------------*/
 typedef struct tb6560 {
-    pin_clk_t pin_clk;
-    pin_dir_t pin_dir;
-    tb6560_micro_step_t micro_step_div;
+	pin_clk_t *pin_clk;
+	pin_dir_t *pin_dir;
+	tb6560_micro_step_t micro_step_div;
 } tb6560_t;
 
 /* Internal function ---------------------------------------------------------*/
-
+static int tb6560_cleanup(tb6560_handle_t handle);
 
 /* External function ---------------------------------------------------------*/
 tb6560_handle_t tb6560_init(tb6560_config_t *config)
 {
-	pwm_init(&config->pin_clk);
-	gpio_output_init(&config->pin_dir);
+	tb6560_handle_t handle;
+	if ( (handle          = calloc(1, sizeof(tb6560_t)))   &&
+	     (handle->pin_clk = calloc(1, sizeof(pin_clk_t)))  &&
+	     (handle->pin_dir = calloc(1, sizeof(pin_dir_t)))  == NULL)
+	{
+		tb6560_cleanup(handle);
+		return -1;
+	}
 
-	tb6560_handle_t handle = calloc(1,sizeof(tb6560_t));
-	handle->pin_clk.pwm_channel     = config->pin_clk.pwm_channel;
-	handle->pin_clk.pwm_pins_pack   = config->pin_clk.pwm_pins_pack;
-	handle->pin_clk.pwm_duty        = config->pin_clk.pwm_duty;
-	handle->pin_clk.timer           = config->pin_clk.timer;
-	handle->pin_clk.timer_period    = config->pin_clk.timer_period;
-	handle->pin_clk.timer_prescaler = config->pin_clk.timer_prescaler;
-	handle->pin_dir.GPIOx           = config->pin_dir.GPIOx;
-	handle->pin_dir.GPIO_Pin        = config->pin_dir.GPIO_Pin;
-	handle->micro_step_div          = config->micro_step_div;
+	handle->pin_clk = (pin_clk_t *)pwm_init(&config->pin_clk);
+	handle->pin_dir = (pin_dir_t *)gpio_output_init(&config->pin_dir);
+	handle->micro_step_div = config->micro_step_div;
 
 	return handle;
+}
+
+int tb6560_cleanup(tb6560_handle_t handle)
+{
+	free(handle->pin_clk);
+	free(handle->pin_dir);
+	free(handle);
+
+	return 0;
 }
