37eb87e381406b5b203174f16c6a1bc4dc0b1791
phonght32 <thanhphongho1998@gmail.com>
2019-12-09 12:12:57 +0700

    add a4988 handle context

--- stm32f407/components/driver_step_motor/a4988.c
+++ stm32f407/components/driver_step_motor/a4988.c
@@ -5,7 +5,11 @@
 
 
 /* Internal typedef ----------------------------------------------------------*/
-
+typedef struct a4988 {
+    pin_clk_t pin_clk;
+    pin_dir_t pin_dir;
+    micro_step_div_t micro_step_div;
+} a4988_t;
 
 /* Internal function ---------------------------------------------------------*/
 
--- stm32f407/components/driver_step_motor/include/a4988.h
+++ stm32f407/components/driver_step_motor/include/a4988.h
@@ -11,11 +11,15 @@ extern "C" {
 #include "../../driver/include/gpio.h"
 #include "step_driver_utils.h"
 
+typedef struct a4988 *a4988_handle_t;
+
 typedef struct {
     pin_clk_t pin_clk;
     pin_dir_t pin_dir;
     micro_step_div_t micro_step_div;
 } a4988_config_t;
+
+
 
 #ifdef __cplusplus
 }
