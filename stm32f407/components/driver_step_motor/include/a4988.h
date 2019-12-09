2c34640c9489001fc84ee9f5766428a983d8cb0c
phonght32 <thanhphongho1998@gmail.com>
2019-12-09 11:41:34 +0700

    add a4988 config structure

--- stm32f407/components/driver_step_motor/include/a4988.h
+++ stm32f407/components/driver_step_motor/include/a4988.h
@@ -1,11 +1,22 @@
 #ifndef A4988_H_
 #define A4988_H_
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 extern "C" {
 #endif
 
 #include "stdint.h"
+
+#include "../../driver/include/timer.h"
+#include "../../driver/include/gpio.h"
+
+typedef gpio_config_t pin_dir_t;
+typedef pwm_config_t pin_clk_t;
+
+typedef struct {
+    pin_clk_t pin_clk;
+    pin_dir_t pin_dir;
+} a4988_config_t;
 
 #ifdef __cplusplus
 }
