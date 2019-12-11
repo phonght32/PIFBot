eb9265b96b550202964f84c813094dbc51cb71e1
phonght32 <thanhphongho1998@gmail.com>
2019-12-11 11:12:53 +0700

    update main.c

--- stm32f407/src/main.c
+++ stm32f407/src/main.c
@@ -17,11 +17,11 @@ int main(void)
 
     tb6560_config_t tb6560_cfg;
     tb6560_cfg.pin_clk.pwm_channel = PWM_CHANNEL_1;
-    tb6560_cfg.pin_clk.pwm_pins_pack = PWM_PINS_PACK_2;
+    tb6560_cfg.pin_clk.pwm_pins_pack = PWM_PINS_PACK_1;
     tb6560_cfg.pin_clk.pwm_duty = 50;
-    tb6560_cfg.pin_clk.timer = TIMER_NUM_4;
-    tb6560_cfg.pin_clk.timer_prescaler = 9;
-    tb6560_cfg.pin_clk.timer_period = 167;
+    tb6560_cfg.pin_clk.timer = TIMER_NUM_1;
+    tb6560_cfg.pin_clk.timer_prescaler = 0;
+    tb6560_cfg.pin_clk.timer_period = 2;
     tb6560_cfg.pin_dir.GPIOx = GPIOD;
     tb6560_cfg.pin_dir.GPIO_Pin = GPIO_Pin_15;
     tb6560_cfg.pin_dir.pull_reg = GPIO_PULL_REG_DISABLE;
