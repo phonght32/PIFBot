2d3d66266cadd7dce2ffc1e2885304fd373d75fd
phonght32 <thanhphongho1998@gmail.com>
2019-12-11 19:03:19 +0700

    update main.c

--- stm32f407/src/main.c
+++ stm32f407/src/main.c
@@ -32,26 +32,27 @@ int main(void)
     a4988_config_t a4988_cfg;
     a4988_cfg.pin_clk.pwm_channel = PWM_CHANNEL_1;
     a4988_cfg.pin_clk.pwm_pins_pack = PWM_PINS_PACK_2;
-    a4988_cfg.pin_clk.pwm_duty = 25;
+    a4988_cfg.pin_clk.pwm_duty = 50;
     a4988_cfg.pin_clk.timer = TIMER_NUM_4;
-    a4988_cfg.pin_clk.timer_prescaler = 48;
-    a4988_cfg.pin_clk.timer_period = 100;
+    a4988_cfg.pin_clk.timer_prescaler = 0;
+    a4988_cfg.pin_clk.timer_period = 0;
     a4988_cfg.pin_dir.GPIOx = GPIOD;
     a4988_cfg.pin_dir.GPIO_Pin = GPIO_Pin_15;
     a4988_cfg.pin_dir.pull_reg = GPIO_PULL_REG_DISABLE;
     a4988_cfg.micro_step_div = MICRO_STEP_DIV16;
     a4988_handle_t a4988_handle = a4988_init(&a4988_cfg);
-    a4988_start(a4988_handle);
+
+    a4988_set_freq(a4988_handle,2000);
     a4988_set_dir(a4988_handle,0);
-
-
+    a4988_start(a4988_handle);
 
     while (1)
     {
-        if(tick_count == 100)
-        {
-            tick_count = 0;
-        }
+    	a4988_stop(a4988_handle);
+    	a4988_toggle_dir(a4988_handle);
+    	delay_01ms(5000);
+    	a4988_start(a4988_handle);
+        delay_01ms(10000);
     }
 
     return 0;
