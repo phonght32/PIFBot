9c5d3c4ff1f23709397b3ccff039a09c226289ed
phonght32 <thanhphongho1998@gmail.com>
2019-12-11 21:31:04 +0700

    update a4988 toggle dir

--- stm32f407/components/driver_step_motor/a4988.c
+++ stm32f407/components/driver_step_motor/a4988.c
@@ -70,7 +70,7 @@ int a4988_stop(a4988_handle_t handle)
 
 int a4988_set_dir(a4988_handle_t handle, uint8_t dir)
 {
-    gpio_set_level((gpio_handle_t *)handle->pin_dir, dir);
+    gpio_set_level(handle->pin_dir, dir);
     handle->dir = dir;
 
     return 0;
@@ -78,22 +78,36 @@ int a4988_set_dir(a4988_handle_t handle, uint8_t dir)
 
 int a4988_toggle_dir(a4988_handle_t handle)
 {
-    gpio_toggle((gpio_handle_t *)handle->pin_dir);
-    handle->dir = !handle->dir;
+	if(handle->status == MODE_RUNNING)
+	{
+		pwm_stop(handle->pin_clk);
+		handle->status = MODE_STOP;
+		gpio_toggle(handle->pin_dir);
+		pwm_start(handle->pin_clk);
+		handle->status = MODE_RUNNING;
+		handle->dir = !handle->dir;
 
-	return 0;
+		return 0;
+	}
+	else
+	{
+		gpio_toggle(handle->pin_dir);
+		handle->dir = !handle->dir;
+
+		return 0;
+	}
 }
 
 int a4988_set_freq(a4988_handle_t handle, uint32_t freq_hz)
 {
-    pwm_set_freq((pin_clk_t *)handle->pin_clk, freq_hz);
+    pwm_set_freq(handle->pin_clk, freq_hz);
 
     return 0;
 }
 
 int a4988_deinit(a4988_handle_t handle)
 {
-    pwm_deinit((pwm_handle_t *)handle->pin_clk);
+    pwm_deinit(handle->pin_clk);
     a4988_cleanup(handle);
 
     return 0;
