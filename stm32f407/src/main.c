69524542fa8915a9ddf61e3c2e344842e7bb633b
phonght32 <thanhphongho1998@gmail.com>
2019-12-11 19:40:54 +0700

    update main.c

--- stm32f407/src/main.c
+++ stm32f407/src/main.c
@@ -16,16 +16,12 @@ int main(void)
     SysTick_Config(SystemCoreClock/100);
 
     pwm_config_t pwm_cfg;
-    pwm_cfg.timer 			= TIMER_NUM_2;
+    pwm_cfg.timer 			= TIMER_NUM_4;
     pwm_cfg.pwm_pins_pack 	= PWM_PINS_PACK_2;
-    pwm_cfg.pwm_channel 	= PWM_CHANNEL_3;
-    pwm_cfg.pwm_duty 		= 50;
-    pwm_cfg.timer_prescaler = 83;
-    pwm_cfg.timer_period 	= 9999;
-
+    pwm_cfg.pwm_channel 	= PWM_CHANNEL_1;
+    pwm_cfg.duty_percent 	= 50;
+    pwm_cfg.freq_hz 		= 1;
     pwm_handle_t pwm_handle = pwm_init(&pwm_cfg);
-    pwm_set_freq(pwm_handle, 100);
-    pwm_set_duty(pwm_handle, 50);
     pwm_start(pwm_handle);
 
 
