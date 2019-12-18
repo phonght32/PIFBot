b6bb3d280eaa3c72a63790fee39148b83b420237
phonght32 <thanhphongho1998@gmail.com>
2019-12-18 21:32:12 +0700

    add gazebo_rviz

--- ros/build/catkin_generated/setup_cached.sh
+++ ros/build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/phong/ros_robot_programming_book/devel/lib:/home/phong/Documents/workspace/workspace_ros/catkin_ws/devel/lib:/home/phong/Documents/workspace/workspace_ros/ros_test/devel/lib:/opt/ros/melodic/lib"
-export PKG_CONFIG_PATH="/home/phong/ros_robot_programming_book/devel/lib/pkgconfig:/home/phong/Documents/workspace/workspace_ros/catkin_ws/devel/lib/pkgconfig:/home/phong/Documents/workspace/workspace_ros/ros_test/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig"
 export PWD="/home/phong/Documents/thesis/ros/build"
-export PYTHONPATH="/home/phong/ros_robot_programming_book/devel/lib/python2.7/dist-packages:/home/phong/Documents/workspace/workspace_ros/catkin_ws/devel/lib/python2.7/dist-packages:/home/phong/Documents/workspace/workspace_ros/ros_test/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages"
---
+++ ros/build/robot/robot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/phong/Documents/thesis/ros/src/robot/robot_bringup
+# Build directory: /home/phong/Documents/thesis/ros/build/robot/robot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
