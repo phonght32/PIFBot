116553b7bf476182aea8a44a6fc7d3c118bdce26
phonght32 <thanhphongho1998@gmail.com>
2019-12-18 20:35:35 +0700

    add robot teleop

---
+++ ros/build/robot/robot_teleop/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/phong/Documents/thesis/ros/src/robot/robot_teleop
+# Build directory: /home/phong/Documents/thesis/ros/build/robot/robot_teleop
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
